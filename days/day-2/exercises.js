20d11aeb8d4beda0da18d54c9695e6034ab2eaa9
Brayden-g-21 <brayden.jgibson15@gmail.com>
2023-11-01 09:34:29 -0600

    completed level-2 exercises

--- days/day-2/exercises.js
+++ days/day-2/exercises.js
@@ -1,4 +1,4 @@
-const challenge = "30 Days of JavaScript";
+/*const challenge = "30 Days of JavaScript";
 console.log(challenge);
 console.log(challenge.length);
 console.log(challenge.toUpperCase());
@@ -16,10 +16,10 @@ console.log(challenge.replace('JavaScript', 'Python'));
 console.log(challenge.charAt(15));
 console.log(challenge.charCodeAt('J'));
 console.log(challenge.indexOf('a'));
-console.log(challenge.lastIndexOf('a'));
+console.log(challenge.lastIndexOf('a'));*/
 
 const because = 'You cannot end a sentence with because because because is a conjunction';
-console.log(because.lastIndexOf('because'));
+/*console.log(because.lastIndexOf('because'));
 console.log(because.search('because'));
 console.log(because.trim());
 
@@ -27,8 +27,33 @@ console.log(challenge.startsWith('3'));
 console.log(challenge.endsWith('t'));
 console.log(challenge.match('a'));
 
-const challenge_1 = '30 Days Of ';
+const challenge_1 = '30 Days Of ';*/
 const challenge_2 = 'JavaScript';
-console.log(challenge_1.concat(challenge_2));
+//console.log(challenge_1.concat(challenge_2));
 
-console.log(challenge.repeat(2));
+//console.log(challenge.repeat(2));
+
+//Exercise 2
+
+console.log('There is no exercise better for the heart than reaching down and lifting people up.');
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+let ten = '10';
+console.log(typeof(ten));
+let numTen = Number(ten);
+console.log(numTen);
+let floatNum = '9.8';
+let newFloatNum = parseFloat(floatNum);
+console.log(Math.ceil(newFloatNum));
+
+const py = 'python';
+const jar = 'jargon';
+console.log(py.includes('on') === jar.includes('on'));
+let jarSentence = 'I hope this course is not full of jargon';
+console.log(jarSentence.includes('I hope this course is not full of jargon'));
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.floor(Math.random() * 50) + 50);
+console.log(Math.ceil(Math.random() * 255));
+
+console.log(challenge_2.charAt(Math.floor(Math.random() * 10)));
+console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
+console.log(because.substring(31, 55));
