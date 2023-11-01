3ad5a732d5a0de9bacb36481482bbdff3188481d
Brayden-g-21 <brayden.jgibson15@gmail.com>
2023-11-01 11:15:25 -0600

    completed exercise level 3

--- days/day-2/exercises.js
+++ days/day-2/exercises.js
@@ -57,3 +57,21 @@ console.log(Math.ceil(Math.random() * 255));
 console.log(challenge_2.charAt(Math.floor(Math.random() * 10)));
 console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
 console.log(because.substring(31, 55));
+
+
+//Level 3 Exercises
+
+let loveSentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+console.log(loveSentence.match('love'));
+let pattern = /because/gi;
+console.log(because.match(pattern));
+
+const badSentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+let newPattern = /[\$\%\@\;\&\#]/gi
+console.log(badSentence.replace(newPattern, ''));
+
+let income = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+monthlySalary = Number(income.substring(9,13));
+onlineMonthlySalary = Number(income.substring(67,72));
+annualBonus = Number(income.substring(42,47));
+console.log((monthlySalary + onlineMonthlySalary) * 12 + annualBonus);
