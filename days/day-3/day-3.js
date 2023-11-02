64bf30c8224cb46a5f3b760843ee9e2ca57db20d
Brayden-g-21 <brayden.jgibson15@gmail.com>
2023-11-01 18:43:37 -0600

    completed day-3

--- days/day-3/day-3.js
+++ days/day-3/day-3.js
@@ -71,3 +71,77 @@ function triangleArea(base, height) {
     return area;
 };
 console.log(triangleArea(20, 10));
+
+function trianglePerimeter(a, b, c) {
+    perimeter = a + b + c;
+    return perimeter;
+};
+console.log(trianglePerimeter(5, 4, 3))
+
+function rectangleStats(width, height) {
+    area = width * height;
+    perimeter = (width + height) * 2;
+    console.log(`Area = ${area}\nPerimeter = ${perimeter}`)
+};
+rectangleStats(5,6);
+
+pi = 3.14
+
+function circleArea(radius) {
+    area = pi * radius * radius;
+    console.log(`Area of the circle is: ${area}`);
+};
+circleArea(5);
+
+function slope(xIntercept) {
+    yIntercept = 2 * xIntercept - 2;
+    console.log(`The slope is: ${yIntercept}`)
+};
+slope(4);
+
+function workerPay(hours, pay) {
+    weeklyPay = hours * pay;
+    console.log(`Hours per week: ${hours}\nRate per hour: ${pay}\nWeekly pay: ${weeklyPay}`);
+};
+workerPay(41, 23);
+
+if (firstName.length >= 7) {
+    console.log('Your name is long!')
+} else {
+    console.log('Your name is short...')
+};
+
+if (firstName.length === lastName.length) {
+    console.log('Your first and last names have the same amount of letters');
+} else if (firstName.length != lastName.length) {
+    console.log('Your first and last name have different numbers of letters');
+};
+
+const yourAge = 87;
+const myAge = 23;
+let yearsOlder = yourAge - myAge;
+console.log(`You are ${yearsOlder} years older than me.`);
+
+function isOfAge(yearBorn) {
+    driverAge = now.getFullYear() - yearBorn;
+    yearsToWait = 18 - driverAge;
+    if (driverAge >= 18) {
+        console.log('You are old enough to drive!');
+    } else {
+        console.log(`You must wait another ${yearsToWait} to drive.`);
+    }
+};
+
+isOfAge(2011);
+
+function secondsToLive(expectedAge) {
+    secondsOfLife = expectedAge * 365.25 * 24 * 60 * 60;
+    console.log(`You expect to live: ${expectedAge} years\nYou will live: ${secondsOfLife} seconds`)
+};
+secondsToLive(80);
+
+console.log(now.toLocaleDateString() + ' - ' + now.toLocaleTimeString());
+
+//Exercises Level 3
+
+console.log(`${now.getHours()}:${now.getMinutes()}`)
